5f266e32a567b94ba737812d43903c0df7ba7d5b
KevonRap <104363810+KevonRap@users.noreply.github.com>
2023-08-15 18:47:29 -0400

    Update MusicBeatSubstate.hx

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -7,6 +7,13 @@ import flixel.input.gamepad.FlxGamepad;
 import Conductor.BPMChangeEvent;
 import flixel.FlxG;
 import flixel.FlxSubState;
+#if mobile
+import mobile.flixel.FlxHitbox;
+import mobile.flixel.FlxVirtualPad;
+import flixel.FlxCamera;
+import flixel.input.actions.FlxActionInput;
+import flixel.util.FlxDestroyUtil;
+#end
 
 class MusicBeatSubstate extends FlxSubState
 {
